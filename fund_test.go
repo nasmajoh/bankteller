be762b1491c7d036ea73df90bd134451f79d7e32
John Nasman <john.nasman@iki.fi>
2015-09-16 20:05:30 +0300

    concurrent testing added

--- fund_test.go
+++ fund_test.go
@@ -1,15 +1,42 @@
 package funding
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
+
+const WORKERS = 10
 
 func BenchmarkFund(b *testing.B) {
+	//skip N=1
+	if b.N < WORKERS {
+		return
+	}
+
 	// Add as many dollars as we have iterations this run (= 2 billion)
 	fund := Newfund(b.N)
 
+	dollarsPerFounder := b.N / WORKERS
+	var wg sync.WaitGroup
+
 	// loop 2 billion times and withdraw one euro
-	for i := 0; i < b.N; i++ {
-		fund.Withdraw(1)
+	for i := 0; i < WORKERS; i++ {
+		// Let the waitgroup know we're adding a goroutine
+		wg.Add(1)
+
+		// Spawn off a founder worker, as a closure (anonymous goroutine)
+		go func() {
+			//mark this worker done when the function finished
+			defer wg.Done()
+
+			for i := 0; i < dollarsPerFounder; i++ {
+				fund.Withdraw(1)
+			}
+		}()
 	}
+
+	// wait for all threads to finish
+	wg.Wait()
 
 	if fund.Balance() != 0 {
 		b.Error("Balance was't zero: ", fund.Balance())
